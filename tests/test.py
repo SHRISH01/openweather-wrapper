33f0edb1ac64e1ddbf34ad5bf56eaf3d0f249a77
Shrish Kamboj <shrishkamboz@gmail.com>
2024-12-17 23:24:05 +0530

    new functions

--- tests/test.py
+++ tests/test.py
@@ -3,4 +3,5 @@ from openweather.weather import OpenWeather
 api_key = "Your API Key"
 weather_api = OpenWeather(api_key)
 
-weather_api.plot_weather(city="Delhi")
+result = weather_api.convert_temperature(300, 'K', 'C')
+print(result)
