764b646f79165cdce2bc9fc70acb00f4f3862727
Shrish Kamboj <shrishkamboz@gmail.com>
2024-12-17 23:47:41 +0530

    test

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 from openweather.weather import OpenWeather
 
-api_key = "2c93339e7879008704993fef099aba90"
+api_key = "Your_API_KEY"
 weather_api = OpenWeather(api_key)
 
-weather_api.create_weather_alert_notification('Delhi')
+#weather_api.....
